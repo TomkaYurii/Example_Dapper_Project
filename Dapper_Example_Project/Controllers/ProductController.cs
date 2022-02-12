96fda479707d21dcff2385642a0b7f7f99587d90
Drudkh <tomka.yurii@gmail.com>
2022-02-13 01:29:24 +0200

    Добавьте файлы проекта.

---
+++ Dapper_Example_Project/Controllers/ProductController.cs
@@ -0,0 +1,71 @@
+﻿using Dapper_Example_Project.Entities;
+using Dapper_Example_Project.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace restapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        private IUnitOfWork _unitofWork;
+        public ProductController(ILogger<ProductController> logger, IUnitOfWork unitofWork)
+        {
+            _logger = logger;
+            _unitofWork = unitofWork;
+        }
+
+        // GET: api/Product
+        [HttpGet]
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            IEnumerable<Product> results = await _unitofWork._productRepository.GetAllAsync();
+            _unitofWork.Commit();
+            return results;
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}")]
+        public async Task<Product> GetAsync(int id)
+        {
+            Product result = await _unitofWork._productRepository.GetAsync(5);
+            _unitofWork.Commit();
+            return result;
+        }
+
+        [HttpGet("category/{category_id}")]
+        public async Task<IEnumerable<Product>> GetByCategoryId(int category_id)
+        {
+            IEnumerable<Product> results = await _unitofWork._productRepository.ProductByCategoryASync(category_id);
+            _unitofWork.Commit();
+            return results;
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public async Task Post([FromBody] Product newProduct)
+        {
+            await _unitofWork._productRepository.AddAsync(newProduct);
+            _unitofWork.Commit();
+        }
+
+        [HttpPut]
+        public async Task Put([FromBody] Product updateProduct)
+        {
+            await _unitofWork._productRepository.ReplaceAsync(updateProduct);
+            _unitofWork.Commit();
+
+        }
+
+        [HttpDelete]
+        public async Task Delete(int id)
+        {
+            await _unitofWork._productRepository.DeleteAsync(id);
+            _unitofWork.Commit();
+        }
+    }
+}
+
---
+++ Dapper_Example_Project/Program.cs
@@ -0,0 +1,45 @@
+using Dapper_Example_Project.Repositories;
+using Dapper_Example_Project.Repositories.Interfaces;
+using System.Data;
+using System.Data.SqlClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitofWork>();
+
+
+builder.Services.AddScoped((s) => new SqlConnection(builder.Configuration.GetConnectionString("MSSQLConnection")));
+builder.Services.AddScoped<IDbTransaction>(s =>
+{
+    SqlConnection conn = s.GetRequiredService<SqlConnection>();
+    conn.Open();
+    return conn.BeginTransaction();
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
