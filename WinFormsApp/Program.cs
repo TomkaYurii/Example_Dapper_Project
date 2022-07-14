b04fde367b258119000b66f385d22636277d9e99
Tomka Yuriy <tomka.yurii@gmail.com>
2022-07-14 12:40:34 +0300

    added web project

--- WinFormsApp/Program.cs
+++ WinFormsApp/Program.cs
@@ -48,9 +48,7 @@ namespace WinFormsApp
             using IServiceScope serviceScope = host.Services.CreateScope();
             IServiceProvider provider = serviceScope.ServiceProvider;
             var formSVC = provider.GetRequiredService<Form1>();
-            // To customize application configuration such as set high DPI settings or default font,
-            //// see https://aka.ms/applicationconfiguration.
-            //ApplicationConfiguration.Initialize();
+
             Application.Run(formSVC);
         }
     }
